c5cc1f63b32dd554ff02311fcf80c9c0cc76096c
ewen <ewen.grosjean@gmail.com>
2015-04-01 01:00:50 +0200

    Fix clojure jars listing

--- replique.el
+++ replique.el
@@ -39,37 +39,36 @@
   '("project.clj" "build.boot")
   "A list of files that can be considered project markers.")
 
-
-(let* ((normalize-strings (lambda (x y)
-                            (let* ((diff-length (- (length x) (length y)))
-                                   (abs-diff-length (abs diff-length)))
-                              (cond ((equal 0 diff-length) (list x y))
-                                    ((< diff-length 0) (list (concat x (make-string abs-diff-length ?0)) y))
-                                    ((> diff-length 0) (list x (concat y (make-string abs-diff-length ?0))))))))
-       (release-type< (lambda (x y)
-                       (cond ((equal x y) nil)
-                             ((equal x nil) nil)
-                             ((equal y nil) t)
-                             ((and (equal x "SNAPSHOT") (equal y "alpha")) t)
-                             ((and (equal x "SNAPSHOT") (equal y "beta")) t)
-                             ((and (equal x "alpha") (equal y "beta")) t)
-                             ((and (equal y "SNAPSHOT") (equal x "alpha")) nil)
-                             ((and (equal y "SNAPSHOT") (equal x "beta")) nil)
-                             ((and (equal y "alpha") (equal x "beta")) nil)
-                             (t nil))))
-       (jar-version< (lambda (x y)
-                       (let ((normalized-versions (funcall normalize-strings (car x) (car y))))
-                         (cond ((and (equal (car normalized-versions) (cadr normalized-versions))
-                                     (equal (nth 1 x) (nth 1 y)))
-                                (string< (nth 2 x) (nth 2 y)))
-                               ((equal (car normalized-versions) (cadr normalized-versions))
-                                (funcall release-type< (nth 1 x) (nth 1 y)))
-                               (t (string< (car x) (car y)))))))
-      (jar-version> (lambda (x y)
-                      (not (funcall jar-version< x y))))
-      (clojure-jars (split-string
-                     (shell-command-to-string
-                      "for dir in `echo $PATH | sed \"s/:/ /g\"`
+(defun replique-clojure-jars-in-path ()
+  (let* ((normalize-strings (lambda (x y)
+                              (let* ((diff-length (- (length x) (length y)))
+                                     (abs-diff-length (abs diff-length)))
+                                (cond ((equal 0 diff-length) (list x y))
+                                      ((< diff-length 0) (list (concat x (make-string abs-diff-length ?0)) y))
+                                      ((> diff-length 0) (list x (concat y (make-string abs-diff-length ?0))))))))
+         (release-type< (lambda (x y)
+                          (cond ((equal x y) nil)
+                                ((equal x nil) nil)
+                                ((equal y nil) t)
+                                ((and (equal x "SNAPSHOT") (equal y "alpha")) t)
+                                ((and (equal x "SNAPSHOT") (equal y "beta")) t)
+                                ((and (equal x "alpha") (equal y "beta")) t)
+                                ((and (equal y "SNAPSHOT") (equal x "alpha")) nil)
+                                ((and (equal y "SNAPSHOT") (equal x "beta")) nil)
+                                ((and (equal y "alpha") (equal x "beta")) nil)
+                                (t nil))))
+         (jar-version< (lambda (x y)
+                         (let ((normalized-versions (funcall normalize-strings (car x) (car y))))
+                           (cond ((and (equal (car normalized-versions) (cadr normalized-versions))
+                                       (equal (nth 1 x) (nth 1 y)))
+                                  (string< (nth 2 x) (nth 2 y)))
+                                 ((equal (car normalized-versions) (cadr normalized-versions))
+                                  (funcall release-type< (nth 1 x) (nth 1 y)))
+                                 (t (string< (car x) (car y)))))))
+         (jar-version> (lambda (x y)
+                         (not (funcall jar-version< x y))))
+         (shell-command-out (shell-command-to-string
+                             "for dir in `echo $PATH | sed \"s/:/ /g\"`
 do
     if [ -d \"$dir\" ];
     then JARS=`find $dir -maxdepth 1 -name \"clojure-*.jar\"`;
@@ -80,14 +79,19 @@ do
     then echo -n $JARS;
     fi
 done
-")
-                     " ")))
-  (let ((clojure-jar-versions (save-match-data
-                                (mapcar (lambda (x)
-                                          (string-match "clojure-\\([[:digit:].]+\\)-?\\(beta\\|alpha\\|SNAPSHOT\\)?\\([0-9]+\\)?.jar" x)
-                                          (list (or (match-string 1 x) "0") (match-string 2 x) (or (match-string 3 x) "0")))
-                                        clojure-jars))))
-    (sort clojure-jar-versions jar-version>)))
+"))
+         (clojure-jars (if (> (length shell-command-out) 0)
+                           (split-string
+                            shell-command-out
+                            " ")
+                         nil)))
+    (let ((clojure-jar-versions (save-match-data
+                                  (mapcar (lambda (x)
+                                            (string-match "clojure-\\([[:digit:].]+\\)-?\\(beta\\|alpha\\|SNAPSHOT\\)?\\([0-9]+\\)?.jar" x)
+                                            (list (or (match-string 1 x) "0") (match-string 2 x) (or (match-string 3 x) "0")))
+                                          clojure-jars))))
+      (sort clojure-jar-versions jar-version>))))
+
 
 
 
