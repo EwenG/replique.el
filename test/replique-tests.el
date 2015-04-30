119253c14d58e33a6ee0bbe9ba2607e5708cc4aa
egr <egr@zengularity.com>
2015-04-30 18:34:06 +0200

    Indent code

--- test/replique-tests.el
+++ test/replique-tests.el
@@ -12,20 +12,36 @@
                  (cons jar-1 '("1.6.00" "alpha" "45")))))
 
 (ert-deftest test-replique-sort-jars ()
-  (setq-local clojure-jars-1 '("/home/ewen/bin/clojure-1.6.00-alpha45.jar" "/home/ewen/bin/clojure-1.6.00.jar" "/home/ewen/bin/clojure-1.2-beta.jar" "/home/ewen/bin/clojure-1.2.2-SNAPSHOT.jar" "/home/ewen/bin/clojure-1.6.0.jar"))
-  (let* ((clojure-jar-versions-1 (mapcar 'replique-clojure-jar->version clojure-jars-1))
-         (sorted-clojure-jar-versions-1 (sort (copy-tree clojure-jar-versions-1)
-                                              (lambda (x y)
-                                                (replique-jar-version> (cdr x) (cdr y))))))
+  (setq-local clojure-jars-1 '("/home/ewen/bin/clojure-1.6.00-alpha45.jar"
+                               "/home/ewen/bin/clojure-1.6.00.jar"
+                               "/home/ewen/bin/clojure-1.2-beta.jar"
+                               "/home/ewen/bin/clojure-1.2.2-SNAPSHOT.jar"
+                               "/home/ewen/bin/clojure-1.6.0.jar"))
+  (let* ((clojure-jar-versions-1
+          (mapcar 'replique-clojure-jar->version clojure-jars-1))
+         (sorted-clojure-jar-versions-1
+          (sort (copy-tree clojure-jar-versions-1)
+                (lambda (x y)
+                  (replique-jar-version> (cdr x) (cdr y))))))
     (should (equal clojure-jar-versions-1
-                   (list (cons "/home/ewen/bin/clojure-1.6.00-alpha45.jar" '("1.6.00" "alpha" "45"))
-                         (cons "/home/ewen/bin/clojure-1.6.00.jar" '("1.6.00" nil "0"))
-                         (cons "/home/ewen/bin/clojure-1.2-beta.jar" '("1.2" "beta" "0"))
-                         (cons "/home/ewen/bin/clojure-1.2.2-SNAPSHOT.jar" '("1.2.2" "SNAPSHOT" "0"))
-                         (cons "/home/ewen/bin/clojure-1.6.0.jar" '("1.6.0" nil "0")))))
+                   (list (cons "/home/ewen/bin/clojure-1.6.00-alpha45.jar"
+                               '("1.6.00" "alpha" "45"))
+                         (cons "/home/ewen/bin/clojure-1.6.00.jar"
+                               '("1.6.00" nil "0"))
+                         (cons "/home/ewen/bin/clojure-1.2-beta.jar"
+                               '("1.2" "beta" "0"))
+                         (cons "/home/ewen/bin/clojure-1.2.2-SNAPSHOT.jar"
+                               '("1.2.2" "SNAPSHOT" "0"))
+                         (cons "/home/ewen/bin/clojure-1.6.0.jar"
+                               '("1.6.0" nil "0")))))
     (should (equal sorted-clojure-jar-versions-1
-                   (list (cons "/home/ewen/bin/clojure-1.6.0.jar" '("1.6.0" nil "0"))
-                         (cons "/home/ewen/bin/clojure-1.6.00.jar" '("1.6.00" nil "0"))
-                         (cons "/home/ewen/bin/clojure-1.6.00-alpha45.jar" '("1.6.00" "alpha" "45"))
-                         (cons "/home/ewen/bin/clojure-1.2.2-SNAPSHOT.jar" '("1.2.2" "SNAPSHOT" "0"))
-                         (cons "/home/ewen/bin/clojure-1.2-beta.jar" '("1.2" "beta" "0")))))))
+                   (list (cons "/home/ewen/bin/clojure-1.6.0.jar"
+                               '("1.6.0" nil "0"))
+                         (cons "/home/ewen/bin/clojure-1.6.00.jar"
+                               '("1.6.00" nil "0"))
+                         (cons "/home/ewen/bin/clojure-1.6.00-alpha45.jar"
+                               '("1.6.00" "alpha" "45"))
+                         (cons "/home/ewen/bin/clojure-1.2.2-SNAPSHOT.jar"
+                               '("1.2.2" "SNAPSHOT" "0"))
+                         (cons "/home/ewen/bin/clojure-1.2-beta.jar"
+                               '("1.2" "beta" "0")))))))
