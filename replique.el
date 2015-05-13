040b0ab2a9f7eedcb15d8a517548be5751328f62
egr <egr@zengularity.com>
2015-05-13 22:49:59 +0200

    Delete the replique-comint file for now

--- replique.el
+++ replique.el
@@ -7,7 +7,6 @@
 (require 'dash)
 (require 's)
 (require 'comint)
-(require 'replique-comint)
 (require 'clojure-mode)
 
 
@@ -73,7 +72,43 @@
 
 
 
+(defun replique/comint-is-closed-sexpr (start limit)
+  (let ((depth (car (parse-partial-sexp start limit))))
+    (if (<= depth 0) t nil)))
 
+(defun replique/comint-send-input (&optional no-newline artificial)
+  (interactive)
+  (let ((proc (get-buffer-process (current-buffer))))
+    (if (not proc) (user-error "Current buffer has no process")
+      (widen)
+      (let* ((pmark (process-mark proc)))
+        (cond ((and (equal (point) (point-max))
+                    (replique/comint-is-closed-sexpr pmark (point)))
+               (comint-send-input no-newline artificial))
+              ((comint-after-pmark-p) (comint-accumulate))
+              (t (comint-send-input no-newline artificial)))))))
+
+(defun replique/comint-send-input-from-source (input &optional no-newline artificial)
+  (interactive)
+  (let ((proc (get-buffer-process (current-buffer))))
+    (if (not proc) (user-error "Current buffer has no process")
+      (widen)
+      (comint-add-to-input-history input)
+      (run-hook-with-args 'comint-input-filter-functions
+                          (if no-newline input
+                            (concat input "\n")))
+
+      (comint-snapshot-last-prompt)
+
+      (setq comint-save-input-ring-index comint-input-ring-index)
+      (setq comint-input-ring-index nil)
+
+      (let ((comint-input-sender-no-newline no-newline))
+        (funcall comint-input-sender proc input))
+
+      ;; This used to call comint-output-filter-functions,
+      ;; but that scrolled the buffer in undesirable ways.
+      (run-hook-with-args 'comint-output-filter-functions ""))))
 
 (defun replique/comint-input-sender (proc string)
   (comint-simple-send proc
@@ -92,8 +127,8 @@
 
 (define-derived-mode replique/mode comint-mode "Replique"
   "Commands:\\<replique/mode-map>
-\\[replique/comint-send-input] after the end of the process' output sends the text from the
-    end of process to point."
+\\[replique/comint-send-input] after the end of the process' output sends
+the text from the end of process to point."
   (setq comint-prompt-regexp replique/prompt)
   (setq comint-read-only t)
   (setq comint-input-sender #'replique/comint-input-sender)
