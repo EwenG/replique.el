ae49cccdb7f7e2df26288ce8f7ed5449029fbd26
egr <egr@zengularity.com>
2015-09-08 00:42:51 +0200

    replique-sass

--- replique-sass/main.cpp
+++ replique-sass/main.cpp
@@ -1,29 +1,75 @@
 #include <iostream>
+#include <string.h>
 #include "sass_context.h"
 
 using namespace std;
 
 int main( int argc, const char* argv[] ) {
-    // get the input file from first argument or use default
-    const char* input = argc > 1 ? argv[1] : "styles.scss";
-    // create the file context and get all related structs
-    struct Sass_File_Context* file_ctx = sass_make_file_context(input);
-    struct Sass_Context* ctx = sass_file_context_get_context(file_ctx);
-    struct Sass_Options* ctx_opt = sass_context_get_options(ctx);
+    if (argc != 5) {
+        cout << R"(
+{
+  "status: 5,
+  "message": Invalid number of parameters
+}
+)" << endl;
+        return 5;
+    }
 
-    // configure some options ...
-    sass_option_set_precision(ctx_opt, 10);
+    // get the input sass text from first argument
+    const char* tmpInput = argv[1];
+    char* input = new char[strlen(tmpInput)];
+    strcpy(input, tmpInput);
 
-    // context is set up, call the compile step now
-    int status = sass_compile_file_context(file_ctx);
+    // get the input path
+    const char* input_path = argv[2];
 
-    // print the result or the error to the stdout
-    if (status == 0) puts(sass_context_get_output_string(ctx));
-    else puts(sass_context_get_error_message(ctx));
+    // get the output path
+    const char* output_path = argv[3];
 
-    // release allocated memory
-    sass_delete_file_context(file_ctx);
+    // get the include path
+    const char* include_path = argv[4];
+
+    // the sourcemap file
+    char* source_map_file = new char[strlen(output_path) + 3];
+    strcpy(source_map_file, output_path);
+    strcat(source_map_file, ".map");
+
+    //Create the file context and get all related structs
+    struct Sass_Data_Context* data_context = sass_make_data_context(input);
+    struct Sass_Context* context = sass_data_context_get_context(data_context);
+    struct Sass_Options* options = sass_data_context_get_options(data_context);
+
+    //Set options
+    sass_option_set_precision(options, 10);
+    sass_option_set_source_comments(options, false);
+    sass_data_context_set_options(data_context, options);
+    sass_option_set_input_path(options, input_path);
+    sass_option_set_output_path(options, output_path);
+    sass_option_set_source_map_embed(options,true);
+    sass_option_set_source_map_contents(options,true);
+    sass_option_set_omit_source_map_url(options,false);
+    sass_option_set_include_path (options, include_path);
+    sass_option_set_source_map_file(options, source_map_file);
+
+    sass_compile_data_context(data_context);
+
+    const char* output = sass_context_get_output_string(context);
+    const char* source_map_output = sass_context_get_source_map_string(context);
+
+    int error_status = sass_context_get_error_status(context);
+    const char* json_error = sass_context_get_error_json(context);
+
+    if (error_status > 0) {
+        cout << json_error << endl;
+    } else {
+        cout << output << endl;
+        //cout << source_map_output << endl;
+    }
+
+    // Release memory dedicated to the C compiler
+    sass_delete_data_context(data_context);
+    delete source_map_file;
 
     // exit status
-    return status;
+    return error_status;
 }
