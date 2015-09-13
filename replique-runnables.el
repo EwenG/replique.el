4024a03b9f91e718c9392de7c975ede6b8adf669
egr <egr@zengularity.com>
2015-09-13 18:24:08 +0200

    Show download status when downloading something

--- replique-runnables.el
+++ replique-runnables.el
@@ -94,19 +94,38 @@
 
 ;; Jar downloading
 
-(defun replique-runnables/url-retrieve (url callback)
-  (url-retrieve
-   url
-   (lambda (status)
-     (cond ((null status)
-            (funcall callback status))
-           ((not (null (plist-get status :redirect)))
-            (kill-buffer)
-            (sleep-for 1)
-            (replique-runnables/url-retrieve
-             (plist-get status :redirect)
-             callback))
-           (t (error "Error while downloading %s" status))))))
+(defun replique-runnables/parse-http-headers (buffer)
+  (with-current-buffer buffer
+    (cond ((> (buffer-size buffer) 0)
+           (beginning-of-buffer)
+           (let ((beg (point)))
+             (search-forward-regexp "^$")
+             (forward-char)
+             (save-match-data
+               (let* ((header (buffer-substring-no-properties beg (point)))
+                      (length (if header
+                                  (string-match
+                                   "^Content-Length:\\(.+\\)$" header)))
+                      (length (if length
+                                  (match-string-no-properties 1 header)))
+                      (location (if header
+                                    (string-match
+                                     "^Location:\\(.+\\)$" header)))
+                      (location (if location
+                                    (match-string-no-properties
+                                     1 header))))
+                 (list (if length
+                           (parse-integer (string-trim length))
+                         nil)
+                       (if location
+                           (string-trim location)
+                         nil))))))
+          (t '(nil nil)))))
+
+(defun replique-runnables/get-http-buffer-length (buffer)
+  (with-current-buffer buffer
+    (end-of-buffer)
+    (point)))
 
 (defun replique-runnables/remove-http-header ()
   (beginning-of-buffer)
@@ -114,6 +133,71 @@
     (search-forward-regexp "^$")
     (forward-char)
     (delete-region beg (point))))
+
+(defun replique-runnables/download-ratio (length content-length)
+  (format "%d%s"
+          (truncate (* (/ (float length) (float content-length)) 100))
+          "%"))
+
+(defun replique-runnables/print-download-progress (buffer url)
+  (let ((content-length
+         (car (replique-runnables/parse-http-headers buffer)))
+        (length
+         (replique-runnables/get-http-buffer-length buffer)))
+    (if content-length
+        (message "Downloading %s ... %s"
+                 url
+                 (replique-runnables/download-ratio
+                  length content-length))
+      (message "Downloading %s ... %s" url "0%"))))
+
+(defun replique-runnables/handle-redirects (buffer callback)
+  (cadr (replique-runnables/parse-http-headers buffer)))
+
+(defun replique-runnables/url-retrieve (url done-fn)
+  (let* ((timer nil)
+         (download-buffer
+         (url-retrieve
+          url
+          (lambda (status)
+            (cancel-timer timer)
+            (cond ((null status)
+                   (funcall done-fn status))
+                  ((not (null (plist-get status :redirect)))
+
+                   ;; We would normally handle redirects here.
+                   ;; But the status code is sometimes very long to
+                   ;; receive! (no idea why).
+                   ;; So redirects are handled by reading the "Location"
+                   ;; entry of HTTP headers instead.
+
+                   ;; (kill-buffer)
+                   ;; (sleep-for 1)
+                   ;; (replique-runnables/url-retrieve
+                   ;;  (plist-get status :redirect)
+                   ;;  done-fn)
+                   )
+                  (t
+                   (kill-buffer)
+                   (error "Error while downloading %s" status))))
+          nil t)))
+    (setq timer (run-at-time
+                 0 2
+                 (lambda (buffer)
+                   (if (not (buffer-live-p buffer))
+                       (cancel-timer timer)
+                     (let ((location
+                            (replique-runnables/handle-redirects
+                             buffer done-fn)))
+                       (if location
+                           (progn
+                             (cancel-timer timer)
+                             (kill-buffer buffer)
+                             (replique-runnables/url-retrieve
+                              location done-fn))
+                         (replique-runnables/print-download-progress
+                          buffer url)))))
+                 download-buffer))))
 
 (defun replique-runnables/download-jar (replique-dir platform callback)
   (let* ((url (cond ((equal platform "clj") replique-runnables/clj-url)
@@ -127,21 +211,20 @@
                           ((equal platform "cljs")
                            replique-runnables/cljs-file-name)
                           (t (error "Unsupported platform: %s"
-                                    platform))))))
-    (replique-runnables/url-retrieve
-     url
-     (lambda (status)
-       (replique-runnables/remove-http-header)
-       (ignore-errors
-         (write-file jar-path t))
-       (kill-buffer)
-       (eshell-command-result
-        (format "ln -s %s %s"
-                jar-path
-                (replique-runnables/platform-jar-path
-                 replique-dir platform)))
-       (funcall callback (replique-runnables/platform-jar-path
-                          replique-dir platform))))
+                                    platform)))))
+         (done-fn (lambda (status)
+                    (replique-runnables/remove-http-header)
+                    (ignore-errors
+                      (write-file jar-path t))
+                    (kill-buffer)
+                    (eshell-command-result
+                     (format "ln -s %s %s"
+                             jar-path
+                             (replique-runnables/platform-jar-path
+                              replique-dir platform)))
+                    (funcall callback (replique-runnables/platform-jar-path
+                                       replique-dir platform)))))
+    (replique-runnables/url-retrieve url done-fn)
     jar-path))
 
 (defun replique-runnables/download-sass (replique-dir callback)
@@ -150,18 +233,21 @@
                      "runnables/"
                      (url-file-nondirectory
                       replique-runnables/sass-url)))
-         (sym-link-path (concat replique-dir "runnables/replique_sass")))
-    (replique-runnables/url-retrieve
-     replique-runnables/sass-url
-     (lambda (status)
-       (replique-runnables/remove-http-header)
-       (ignore-errors
-         (write-file sass-path t))
-       (kill-buffer)
-       (eshell-command (concat "chmod +x " sass-path))
-       (eshell-command
-        (format "ln -s %s %s" sass-path sym-link-path))
-       (funcall callback sass-path)))
+         (sym-link-path (concat replique-dir "runnables/replique_sass"))
+         (download-buffer
+          (replique-runnables/url-retrieve
+           replique-runnables/sass-url
+           (lambda (status)
+             (replique-runnables/remove-http-header)
+             (ignore-errors
+               (write-file sass-path t))
+             (kill-buffer)
+             (eshell-command (concat "chmod +x " sass-path))
+             (eshell-command
+              (format "ln -s %s %s" sass-path sym-link-path))
+             (funcall callback sass-path)))))
+    (replique-runnables/print-download-progress
+     download-buffer replique-runnables/sass-url)
     sass-path))
 
 (provide 'replique-runnables)
