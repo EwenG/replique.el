063cbf725d13a4e79cb6f7a62998ad6e68455569
ewen <ewen.grosjean@free.fr>
2016-10-04 00:29:37 +0200

    clj/cljs versions

--- clj/project.clj
+++ clj/project.clj
@@ -1,6 +1,6 @@
 (defproject ewen.replique/replique "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
-                 [org.clojure/clojurescript "1.9.227"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
+                 [org.clojure/clojurescript "1.9.229"]]
   :source-paths ["src"]
   :global-vars {*print-length* 20
                 *print-level* 5})
