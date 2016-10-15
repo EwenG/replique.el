6c1bef1599f0b8fe1cb1211fb90f2b78278a6066
ewen <ewen.grosjean@free.fr>
2016-10-15 15:29:51 +0200

    fix candidates for map-spec, implement candidates for multi-specs

--- clj/src/ewen/replique/spec_tooling.clj
+++ clj/src/ewen/replique/spec_tooling.clj
@@ -9,6 +9,7 @@
 
 (declare candidates)
 (declare form->spec)
+(declare map-spec-impl)
 
 (defprotocol Complete
   (candidates* [spec context prefix]))
@@ -41,14 +42,6 @@
       (set-candidates f context prefix)
       f)))
 
-(extend-protocol Complete
-  nil
-  (candidates* [spec context prefix] nil))
-
-(extend-protocol Complete
-  Object
-  (candidates* [spec context prefix] nil))
-
 (defn complete? [x]
   (when (instance? ewen.replique.spec_tooling.Complete x) x))
 
@@ -70,6 +63,13 @@
 (defn amp-form->spec [pred preds]
   {::s/op ::s/amp :p1 (form->spec pred) :ps (mapv form->spec preds)})
 
+(defn keys-form->spec [keys-seq]
+  (-> (apply hash-map keys-seq)
+      map-spec-impl))
+
+(defn multi-form->spec [mm]
+  (multi-spec-impl (resolve mm)))
+
 ;; Form is a spec OR form is a qualified symbol and can be resolved to a var OR form is something
 ;; that get evaled in order to get the values than the ones from core.spec spec impls.
 ;; Qualified symbols are not evaled in order to customize the candidates* behavior for vars
@@ -87,6 +87,10 @@
                   (rep-form->spec (first spec-rest))
                   (= 'clojure.spec/& spec-sym)
                   (amp-form->spec (first spec-rest) (drop 1 spec-rest))
+                  (= 'clojure.spec/keys spec-sym)
+                  (keys-form->spec spec-rest)
+                  (= 'clojure.spec/multi-spec spec-sym)
+                  (multi-form->spec (first spec-rest))
                   :else (eval form)))
           (c/and (symbol? form) (namespace form) (var? (resolve form)))
           (resolve form)
@@ -137,50 +141,69 @@
   (reify
     Specize
     (specize* [s] s)
-    (specize* [s _] s)
     Complete
     (candidates* [_ [{:keys [idx form]} & _ :as context] prefix]
-      (if (coll? form)
-        (deriv re context prefix)
-        nil))))
+      (when (coll? form)
+        (deriv re context prefix)))))
 
-(defn spec-impl
-  ([pred]
-   (cond
-     #_(spec? pred) #_(cond-> pred gfn (with-gen gfn))
-     (s/regex? pred) (regex-spec-impl pred)
-     #_(ident? pred) #_(cond-> (the-spec pred) gfn (with-gen gfn))
-     :else
-     (reify
-       Specize
-       (specize* [s] s)
-       (specize* [s _] s)
-       
-       Complete
-       (candidates* [_ context prefix]
-         (let [ret (candidates* pred context prefix)]
-           (when (set? ret) ret)))))))
+(defn spec-impl [pred]
+  (cond
+    (s/regex? pred) (regex-spec-impl pred)
+    :else
+    (reify
+      Specize
+      (specize* [s] s)
+      Complete
+      (candidates* [_ context prefix]
+        (let [ret (candidates* pred context prefix)]
+          (when (set? ret) ret))))))
+
+(defn map-spec-impl [{:keys [req-un opt-un req opt]}]
+  (let [req-un->req (zipmap (map (comp keyword name) req-un) req-un)
+        opt-un->opt (zipmap (map (comp keyword name) opt-un) opt-un)
+        unqualified->qualified (c/merge req-un->req opt-un->opt)]
+    (reify
+      Specize
+      (specize* [s] s)
+      Complete
+      (candidates* [_ [{:keys [idx map-role form]} & cs :as context] prefix]
+        (when (map? form)
+          (case map-role
+            :value (when-let [spec (s/get-spec (c/or (get unqualified->qualified idx) idx))]
+                     (candidates spec cs prefix))
+            :key (candidates* (into #{} (concat (c/keys unqualified->qualified) req opt))
+                              nil prefix)))))))
+
+(defn multi-spec-impl [mm]
+  (reify
+    Specize
+    (specize* [s] s)
+    Complete
+    (candidates* [_ [{:keys [form]} & cs :as context] prefix]
+      (let [spec (try ((mm form) nil)
+                      (catch IllegalArgumentException e nil))
+            specs (if spec #{spec} (->> (vals (methods @mm))
+                                        (map (fn [spec-fn] (spec-fn nil)) )
+                                        (into #{})))]
+        (->> (map #(candidates % context prefix) specs)
+             (apply clojure.set/union))))))
 
 (extend-protocol Specize
   clojure.lang.Keyword
-  (specize* ([k] (specize* (reg-resolve! k)))
-            ([k _] (specize* (reg-resolve! k))))
+  (specize* [k] (specize* (reg-resolve! k)))
 
   clojure.lang.Symbol
-  (specize* ([s] (specize* (reg-resolve! s)))
-            ([s _] (specize* (reg-resolve! s))))
+  (specize* [s] (specize* (reg-resolve! s)))
 
-  Object
-  (specize*
-    ([o] (spec-impl o))
-    ([o form] (spec-impl o))))
+  clojure.lang.IFn
+  (specize* [ifn] (spec-impl ifn))
 
-(defn- specize
-  ([s] (c/or (complete? s) (specize* s)))
-  ([s form] (c/or (complete? s) (specize* s form))))
+  clojure.spec.Spec
+  (specize* [spec] (-> (s/form spec) form->spec)))
 
 (defn candidates [spec context prefix]
-  (candidates* (specize spec) context prefix))
+  (when (satisfies? Specize spec)
+    (candidates* (specize* spec) context prefix)))
 
 
 
@@ -190,7 +213,6 @@
   (require '[ewen.replique.replique-conf :as conf])
 
   (candidates "" '({:idx nil, :map-role :key, :form {__prefix__ nil}}) "eee")
-  
   
   )
 
@@ -221,7 +243,7 @@
   (candidates (-> (s/cat :a (s/spec #{11111}))
                   s/form
                   form->spec)
-              '({:idx 0, :form [__prefix__]})
+              '({:idx 2, :form [nil nil __prefix__]})
               "111")
 
   (candidates (-> (s/* (s/cat :a (s/alt :b string? :c #{1111})))
@@ -242,7 +264,43 @@
               '({:idx 0, :form [__prefix__]})
               "eee")
 
-  (form->spec (s/form (s/cat :e (fn [] nil))))
-  
-  )
 
+
+  
+  (candidates (-> (s/keys :req [::ss])
+                  s/form
+                  form->spec)
+              '({:idx ::ss :map-role :value :form {::ss __prefix__}})
+              "11")
+
+  (candidates (-> (s/keys :req [::ss])
+                  s/form
+                  form->spec)
+              '({:idx nil :map-role :key :form {__prefix__ nil}})
+              ":ewen")
+
+  (candidates (s/keys :req-un [::ss])
+              '({:idx nil :map-role :key :form {__prefix__ nil}})
+              ":s")
+
+
+
+  ;; multi spec
+
+  #_(candidates ::conf/cljs-env
+              '({:idx nil :map-role :key :form {__prefix__ nil}})
+              ":ewen")
+
+  (defmulti test-mm :mm)
+  (defmethod test-mm 33 [_]
+    (s/keys :req-un [::ss]))
+  (candidates (s/multi-spec test-mm :mm)
+              '({:idx nil :map-role :key :form {:ss 33
+                                                __prefix__ nil}})
+              ":s")
+  (candidates (s/multi-spec test-mm :ss)
+              '({:idx :ss :map-role :value :form {:mm 33
+                                                  :ss __prefix__}})
+              "11")
+
+  )
