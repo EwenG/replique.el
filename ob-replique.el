297d53af6aad2314bb29b6b9223fc8ade81bf890
ewen <ewen.grosjean@free.fr>
2016-11-19 00:29:05 +0100

    license

--- ob-replique.el
+++ ob-replique.el
@@ -1,8 +1,25 @@
-;;; ob-replique.el ---   -*- lexical-binding: t; -*-
-;;; Package-Requires: ((clojure-mode "4.0.1") (replique "0.0.1") (dash "2.11.0"))
-;;; Commentary:
+;; ob-replique.el ---   -*- lexical-binding: t; -*-
 
-;;; Code:
+;; Copyright © 2016 Ewen Grosjean
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of GNU Emacs.
+
+;; Commentary:
+
+;; Code:
 
 (require 'ob)
 
