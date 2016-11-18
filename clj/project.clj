36fcd70ed1d473230065bf2fde4f66a0f8fe9f7a
ewen <ewen.grosjean@free.fr>
2016-11-19 00:10:05 +0100

    put clojure files in their own repo

--- clj/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject replique/replique "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.229"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
-  :source-paths ["src"]
-  :global-vars {*print-length* 20 *print-level* 5})
