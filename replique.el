1a2dfcd282c8731192b6e25b44aa5d8e2eb84192
ewen <ewen.grosjean@free.fr>
2017-03-12 22:33:38 +0100

    0.0.3

--- replique.el
+++ replique.el
@@ -17,7 +17,7 @@
 
 ;; This file is not part of GNU Emacs.
 
-;; Version 0.0.3-SNAPSHOT
+;; Version 0.0.3
 ;; Package-Requires: ((emacs "25") (clojure-mode "5.6.0"))
 
 ;; Commentary:
@@ -1143,9 +1143,9 @@ This allows you to temporarily modify read-only buffers too."
    (clojurec-mode . (apply-partially 'replique/jump-to-definition-cljc symbol))
    (t . (user-error "Unsupported major mode: %s" major-mode))))
 
-(defconst replique/client-version "0.0.3-SNAPSHOT")
+(defconst replique/client-version "0.0.3")
 
-(defcustom replique/version "0.0.3-SNAPSHOT"
+(defcustom replique/version "0.0.3"
   "Hook for customizing the version of the replique REPL server to be used"
   :type 'string
   :group 'replique)
