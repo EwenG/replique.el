6cb355084bdc32c09b503f4d658175498eebcdea
ewen <ewen.grosjean@free.fr>
2019-02-07 22:37:16 +0100

    0.0.17-SNAPSHOT

--- replique.el
+++ replique.el
@@ -17,7 +17,7 @@
 
 ;; This file is not part of GNU Emacs.
 
-;; Version 0.0.16
+;; Version 0.0.17-SNAPSHOT
 ;; Package-Requires: ((emacs "25") (clojure-mode "5.8.1") (ivy "0.10.0"))
 
 ;; Commentary:
@@ -1065,7 +1065,7 @@
                                               active-repl)))
  )
 
-(defconst replique/version "0.0.16")
+(defconst replique/version "0.0.17-SNAPSHOT")
 
 (defcustom replique/prompt "^[^=> \n]+=> *"
   "Regexp to recognize prompts in the replique mode."
